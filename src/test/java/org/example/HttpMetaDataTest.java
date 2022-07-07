166f32a9b53147b6fa107a18d89b510cdd37a6fd
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2022-07-07 16:50:31 +0300

    Added some tests to MetaData

---
+++ src/test/java/org/example/HttpMetaDataTest.java
@@ -0,0 +1,81 @@
+package org.example;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Suite
+public class HttpMetaDataTest {
+
+
+    private static final String url="jdbc:wm://localhost:8080/sql-mock";
+
+    private static Connection connection ;
+
+    @BeforeAll
+    static void setConnection() throws SQLException{
+
+        connection = DriverManager.getConnection(url);
+
+    }
+
+    @Test
+    public void getDatabaseProductName() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDatabaseProductName());
+
+    }
+
+    @Test
+    public void getDatabaseProductVersion() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDatabaseProductVersion());
+
+    }
+
+    @Test
+    public void getDriverName() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDriverName());
+
+    }
+
+    @Test
+    public void getDriverVersion() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDriverVersion());
+
+    }
+
+    @Test
+    public void getDriverMajorVersion() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDriverMajorVersion());
+
+    }
+
+    @Test
+    public void getDriverMinorVersion() throws SQLException{
+
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        assertNotNull(metaData.getDriverMinorVersion());
+
+    }
+
+}
